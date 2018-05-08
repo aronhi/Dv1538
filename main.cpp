2822858e77933026b4b028ffc37cee5373e53275
ARON HISING <arhi17@student.bth.se>
2018-05-08 12:49:04 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
 	int appa;
 
 	int sven;
 	cout << "hej" << endl;
+	
+	
+	std::string bob = "holla,2,41";
+	cout<<bob.find_last_of(",")<<endl;
+	std::string naaa=bob.substr(8,bob.length());
+	
+	std::cout<<naaa;
 	getchar();
 	return 0;
-
 }
